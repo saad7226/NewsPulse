120873af40a1eb2fd6ca5ad1969b35f868246001
Muhammad Saad Zafar <saadzafar03205501400@gmail.com>
2026-07-28 08:19:30 +0500

    fix: move article_writer DB to /app/data, fix volume mount to stop overriding new code

--- article_writer/database.py
+++ article_writer/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./article_writer.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/article_writer.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
